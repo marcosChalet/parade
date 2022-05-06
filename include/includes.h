fd3cc1fbcb058a15d9c37d41298c21cb95f0d4ab
marcosChalet <chaletmarcos@gmail.com>
2022-05-05 22:39:02 -0300

    Feature: separei a estruct base com os includes em um .h

---
+++ include/includes.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct aluno {
+    char nome[50];
+    int matricula;
+    float av1;
+    float av2;
+    float av3;
+};
+
+#endif /* !INCLUDES_H */
